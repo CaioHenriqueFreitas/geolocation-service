4d3f4dfc652fb3b383e84775a2fd2103c0e26e0d
Caio Freitas <caiohf.cf@gmail.com>
2024-08-22 00:08:31 -0300

    Fixing error treatment

--- src/controllers/distanceController.ts
+++ src/controllers/distanceController.ts
@@ -15,19 +15,7 @@ const getDistance = async (req: Request, res: Response): Promise<void> => {
     const distance = await processDistanceCalculation(origin, destination);
     res.json({ distance });
   } catch (error:unknown) {
-    if (axios.isAxiosError(error)) {
-      // Handle AxiosError specifically
-      logger.error('Axios error message:', error.message);
-      logger.error('Axios error response:', error.response?.data);
-      logger.error('Axios error status:', error.response?.status);
-      logger.error('Axios error headers:', error.response?.headers);
-    } else if (error instanceof Error) {
-      // Handle generic Error
-      logger.error('Error message:', error.message);
-    } else {
-      // Handle unexpected types
-      logger.error('An unexpected error occurred:', error);
-    }
+    logger.error('Error message:', error);
     res.status(500).json({ error: 'Failed to calculate distance' });
   }
 };
