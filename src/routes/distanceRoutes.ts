e41f7e2c73912cc93f6c274fb5ddae47cb92452f
Caio Freitas <caiohf.cf@gmail.com>
2024-08-22 21:18:52 -0300

    Adding CORS policy

--- src/routes/distanceRoutes.ts
+++ src/routes/distanceRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { getDistance } from '../controllers/distanceController';
+import cors from 'cors';
 
 const router: Router = Router();
+
+router.use(cors());
 
 router.get('/', getDistance);
 
